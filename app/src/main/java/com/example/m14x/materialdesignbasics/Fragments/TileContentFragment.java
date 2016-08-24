b785f4ae536dd8a942991ec6eba1688a88f2c152
edBarrientos <edward.barrientos@uicentric.com>
2016-08-24 12:03:17 -0500

    Getting started - Initial commit - Material Design Basics

--- app/src/main/java/com/example/m14x/materialdesignbasics/Fragments/TileContentFragment.java
+++ app/src/main/java/com/example/m14x/materialdesignbasics/Fragments/TileContentFragment.java
@@ -5,7 +5,6 @@ import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
